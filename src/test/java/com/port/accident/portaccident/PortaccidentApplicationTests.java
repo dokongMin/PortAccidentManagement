9035431a9eb0d18923df8cde51f994323db46f05
minhwan <jeff970069@gmail.com>
2022-07-07 13:43:00 +0900

    Initial commit

---
+++ src/test/java/com/port/accident/portaccident/PortaccidentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.port.accident.portaccident;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortaccidentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
