63c48fb592edb5322f2c434916139bed8a837e2f
dclxxi <dclxxi@kakao.com>
2022-08-04 10:53:33 +0900

    feat: modify staff search

---
+++ backend/src/main/java/com/port/accident/portaccident/dto/staff/StaffSearchCondition.java
@@ -0,0 +1,9 @@
+package com.port.accident.portaccident.dto.staff;
+
+import lombok.Data;
+
+@Data
+public class StaffSearchCondition {
+    private String name;
+    private String corporation;
+}
--- backend/src/main/java/com/port/accident/portaccident/repository/staff/StaffRepositoryCustom.java
+++ backend/src/main/java/com/port/accident/portaccident/repository/staff/StaffRepositoryCustom.java
@@ -2,9 +2,10 @@ package com.port.accident.portaccident.repository.staff;
 
 import com.port.accident.portaccident.domain.staff.Staff;
 import com.port.accident.portaccident.dto.SearchCondition;
+import com.port.accident.portaccident.dto.staff.StaffSearchCondition;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface StaffRepositoryCustom {
-    Page<Staff> searchPage(SearchCondition condition, Pageable pageable);
+    Page<Staff> searchPageStaff(StaffSearchCondition condition, Pageable pageable);
 }
