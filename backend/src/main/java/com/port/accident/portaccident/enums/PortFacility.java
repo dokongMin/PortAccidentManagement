86a566f05a96c53ce9353c029567ad3f1989c140
dclxxi <dclxxi@kakao.com>
2022-08-06 16:46:08 +0900

    feat: mapping field value to enum

--- backend/src/main/java/com/port/accident/portaccident/enums/PortFacility.java
+++ backend/src/main/java/com/port/accident/portaccident/enums/PortFacility.java
@@ -1,5 +1,15 @@
 package com.port.accident.portaccident.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum PortFacility {
-    CRANE, CONTAINER, FORKLIFT, LADDER
+    CRANE("크레인"),
+    CONTAINER("컨테이너"),
+    FORKLIFT("지게차"),
+    LADDER("사다리");
+
+    private final String facilityValue;
 }
