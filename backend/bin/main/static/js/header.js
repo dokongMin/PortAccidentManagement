91f3c30f92b7d0b8608164f33d13eade7743e020
joo <twincornjr@gmail.com>
2022-09-29 18:09:15 +0900

    test

--- backend/bin/main/static/js/header.js
+++ backend/bin/main/static/js/header.js
@@ -65,8 +65,8 @@ class PortHeader extends HTMLElement{
             '            <li><a href="#">비상연락망<br>관리</a>\n' +
             '              <ul id="sub-menu">\n' +
             '                <div class="line6"></div>\n' +
-            '                <li><a href="#" aria-label="submenu">비상연락망 조회</a></li>\n' +
-            '                <li><a href="#" aria-label="submenu">비상연락망 등록</a></li>\n' +
+            '                <li><a href="/EmergencyContact/EC_check" aria-label="submenu">비상연락망 조회</a></li>\n' +
+            '                <li><a href="/EmergencyContact/EC_registration_Page" aria-label="submenu">비상연락망 등록</a></li>\n' +
             '                <li><a href="#" aria-label="submenu">비상연락망 수정</a></li>\n' +
             '                <li><a href="#" aria-label="submenu">비상연락망 삭제</a></li>\n' +
             '              </ul>\n' +
@@ -74,7 +74,7 @@ class PortHeader extends HTMLElement{
             '            <li><a href="#">help<br>&nbsp;</a>\n' +
             '              <ul id="sub-menu">\n' +
             '                <div class="line7"></div>\n' +
-            '                <li><a href="#" aria-label="submenu">항만 안전사고 현황</a></li>\n' +
+            '                <li><a href="/help/stateaccident.html" aria-label="submenu">항만 안전사고 현황</a></li>\n' +
             '                <li><a href="#" aria-label="submenu">FAQ</a></li>\n' +
             '                <li><a href="#" aria-label="submenu">자료실</a></li>\n' +
             '              </ul>\n' +
