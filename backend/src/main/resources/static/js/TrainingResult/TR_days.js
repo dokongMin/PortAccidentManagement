4026d105daf46f0b07ab2c1ea26f0c64fbc68789
joo <twincornjr@gmail.com>
2022-10-01 22:57:04 +0900

    docs : modify chart page

--- backend/src/main/resources/static/js/TrainingResult/TR_days.js
+++ backend/src/main/resources/static/js/TrainingResult/TR_days.js
@@ -1,303 +1,95 @@
-const { css } = require("jquery");
 
-function ChangeValue() {
-  const value_str = document.getElementById("choose");
-  const chose = value_str.options[value_str.selectedIndex].value;
-  if (chose == "사고") {
-    const el1 = document.getElementById("ch1");
-    el1.innerHTML = "사고 수준&nbsp;";
-  }
-  if (chose == "재난") {
-    const el1 = document.getElementById("ch1");
-    el1.innerHTML = "재난 수준&nbsp;";
-  }
+
+
+
+
+document.getElementById("daytable2").style.display ='none'; 
+
+function Radio(){
+  var score1 = document.getElementsByName("score1");
+  var sel1 = Array.from(score1).find(radio => radio.checked);
+  const v1=Number(sel1.value);
+  var score2 = document.getElementsByName("score2");
+  var sel2 = Array.from(score2).find(radio => radio.checked);
+  const v2=Number(sel2.value);
+  var score3 = document.getElementsByName("score3");
+  var sel3 = Array.from(score3).find(radio => radio.checked);
+  const v3=Number(sel3.value);
+  const total=document.getElementById("totalsum");
+  const totalsum=v1+v2+v3
+  total.innerHTML=totalsum;
+  if (totalsum<15){
+    const modal = document.querySelector('.modal');
+      const btnOpenPopup = document.querySelector('.btn-open-popup');
+      btnOpenPopup.addEventListener('click', () => {
+        modal.style.display = 'block';
+      });
+  } else {
+    const btnOpenPopup = document.querySelector('.btn-open-popup');
+    btnOpenPopup.addEventListener('click', () => {
+      const success=document.getElementById("success");
+      const word="<div style='color:red;'>합격입니다.</div>"
+      success.innerHTML=word;
+      })   
+  }    
 }
 
-// function change(style) {
-//   if (style == "type1") {
-//     view1.style.display = "inline";
-//     view2.style.display = "inline";
-//     view3.style.display = "inline";
-//     view4.style.display = "inline";
-//     view5.style.display = "inline";
-//     view6.style.display = "inline";
-//     view7.style.display = "inline";
-//     view8.style.display = "none";
-//     view10.style.display = "none";
-//     view11.style.display = "none";
-//     view12.style.display = "none";
-//     view13.style.display = "none";
-//     view14.style.display = "none";
-//   } else if (style == "type2") {
-//     view1.style.display = "none";
-//     view2.style.display = "none";
-//     view3.style.display = "none";
-//     view4.style.display = "none";
-//     view5.style.display = "none";
-//     view6.style.display = "none";
-//     view7.style.display = "none";
-//     view8.style.display = "inline";
-//     view10.style.display = "inline";
-//     view11.style.display = "inline";
-//     view12.style.display = "inline";
-//     view13.style.display = "inline";
-//     view14.style.display = "inline";
-//   }
-// }
+function Evaluation(){
+    const typeC=document.getElementById("typeC");
+    const typeFC = typeC.options[typeC.selectedIndex].value;
+    console.log(typeFC);
+    const levelC=document.getElementById("levelC");
+    const levelFC = levelC.options[levelC.selectedIndex].value;
+    console.log(levelFC);
+    const dayC=document.getElementById("dayC");
+    const dayFC = dayC.options[dayC.selectedIndex].value;
+    console.log(dayFC);
+    if (typeFC=="사고" && levelFC=="level1" && dayFC=="1일차" ){
+      
+      
+    } 
+    if (typeFC=="사고" && levelFC=="level1" && dayFC=="2일차" ){
+      document.getElementById("daytable2").style.display ='block';
+    } 
+    if (typeFC=="사고" && levelFC=="level2" && dayFC=="1일차" ){
 
-// function change1(style1) {
-//   if (style1 == "level1") {
-//     view1.style.display = "inline";
-//     view2.style.display = "inline";
-//     view3.style.display = "none";
-//     view4.style.display = "none";
-//     view5.style.display = "none";
-//     view6.style.display = "none";
-//     view7.style.display = "none";
-//     view8.style.display = "inline";
-//     view10.style.display = "none";
-//     view11.style.display = "none";
-//     view12.style.display = "none";
-//     view13.style.display = "none";
-//     view14.style.display = "none";
-//   }
-//   if (style1 == "level2") {
-//     view1.style.display = "none";
-//     view2.style.display = "none";
-//     view3.style.display = "inline";
-//     view4.style.display = "inline";
-//     view5.style.display = "none";
-//     view6.style.display = "none";
-//     view7.style.display = "none";
-//     view8.style.display = "none";
-//     view10.style.display = "inline";
-//     view11.style.display = "inline";
-//     view12.style.display = "none";
-//     view13.style.display = "none";
-//     view14.style.display = "none";
-//   }
-//   if (style1 == "level3") {
-//     view1.style.display = "none";
-//     view2.style.display = "none";
-//     view3.style.display = "none";
-//     view4.style.display = "none";
-//     view5.style.display = "inline";
-//     view6.style.display = "inline";
-//     view7.style.display = "inline";
-//     view8.style.display = "none";
-//     view10.style.display = "none";
-//     view11.style.display = "none";
-//     view12.style.display = "inline";
-//     view13.style.display = "inline";
-//     view14.style.display = "inline";
-//   }
-// }
-
-// function change2(style2) {
-//   if (style2 == "day1") {
-//     view1.style.display = "inline";
-//     view2.style.display = "none";
-//     view3.style.display = "inline";
-//     view4.style.display = "none";
-//     view5.style.display = "inline";
-//     view6.style.display = "none";
-//     view7.style.display = "none";
-//     view8.style.display = "inline";
-//     view10.style.display = "inline";
-//     view11.style.display = "none";
-//     view12.style.display = "inline";
-//     view13.style.display = "none";
-//     view14.style.display = "none";
-//   }
-//   if (style2 == "day2") {
-//     view1.style.display = "none";
-//     view2.style.display = "inline";
-//     view3.style.display = "none";
-//     view4.style.display = "inline";
-//     view5.style.display = "none";
-//     view6.style.display = "inline";
-//     view7.style.display = "none";
-//     view8.style.display = "none";
-//     view10.style.display = "none";
-//     view11.style.display = "inline";
-//     view12.style.display = "none";
-//     view13.style.display = "inline";
-//     view14.style.display = "none";
-//   }
-//   if (style2 == "day3") {
-//     view1.style.display = "none";
-//     view2.style.display = "none";
-//     view3.style.display = "none";
-//     view4.style.display = "none";
-//     view5.style.display = "none";
-//     view6.style.display = "none";
-//     view7.style.display = "inline";
-//     view8.style.display = "none";
-//     view10.style.display = "none";
-//     view11.style.display = "none";
-//     view12.style.display = "none";
-//     view13.style.display = "none";
-//     view14.style.display = "inline";
-//   }
-// }
-
-function chooseForm(radioName) {
-  var radios = document.getElementsByName(radioName);
-  for (var i = 0, length = radios.length; i < length; i++) {
-    document.getElementById("form_" + radios[i].value).style.display = "none";
-    if (radios[i].checked) {
-      document.getElementById("form_" + radios[i].value).style.display =
-        "block";
     }
-  }
+    if (typeFC=="사고" && levelFC=="level2" && dayFC=="2일차" ){
+
+    }
+    if (typeFC=="사고" && levelFC=="level3" && dayFC=="1일차" ){
+
+    }
+    if (typeFC=="사고" && levelFC=="level3" && dayFC=="2일차" ){
+
+    }
+    if (typeFC=="사고" && levelFC=="level3" && dayFC=="3일차" ){
+
+    } 
+
+    if (typeFC=="재난" && levelFC=="level1" && dayFC=="1일차" ){
+
+    } 
+    if (typeFC=="재난" && levelFC=="level1" && dayFC=="2일차" ){
+
+    } 
+    if (typeFC=="재난" && levelFC=="level2" && dayFC=="1일차" ){
+
+    }
+    if (typeFC=="재난" && levelFC=="level2" && dayFC=="2일차" ){
+
+    }
+    if (typeFC=="재난" && levelFC=="level3" && dayFC=="1일차" ){
+
+    }
+    if (typeFC=="재난" && levelFC=="level3" && dayFC=="2일차" ){
+
+    }
+    if (typeFC=="재난" && levelFC=="level3" && dayFC=="3일차" ){
+
+    }    
+  
 }
 
-function setNumeric(num) {
-  var numeric = Number($.number(num.value));
-  var maxNum = Number(num.dataset.limit);
 
-  if (numeric > maxNum) {
-    alert("최대 점수를 초과하였습니다.");
-    numeric = 0;
-  }
-  num.value = numeric;
-}
 
-function autoCal() {
-  var total = 0;
-  $(".score").each(function () {
-    total += Number($(this).val());
-  });
-  $('input[name="totalScore"]').val(total);
-}
-
-function autoCal2() {
-  var total = 0;
-  $(".score2").each(function () {
-    total += Number($(this).val());
-  });
-  $('input[name="totalScore"]').val(total);
-}
-
-function autoCal3() {
-  var total = 0;
-  $(".score3").each(function () {
-    total += Number($(this).val());
-  });
-  $('input[name="totalScore"]').val(total);
-}
-
-function autoCal4() {
-  var total = 0;
-  $(".score4").each(function () {
-    total += Number($(this).val());
-  });
-  $('input[name="totalScore"]').val(total);
-}
-
-function autoCal5() {
-  var total = 0;
-  $(".score5").each(function () {
-    total += Number($(this).val());
-  });
-  $('input[name="totalScore"]').val(total);
-}
-
-function autoCal6() {
-  var total = 0;
-  $(".score6").each(function () {
-    total += Number($(this).val());
-  });
-  $('input[name="totalScore"]').val(total);
-}
-
-function autoCal7() {
-  var total = 0;
-  $(".score7").each(function () {
-    total += Number($(this).val());
-  });
-  $('input[name="totalScore"]').val(total);
-}
-
-function autoCal8() {
-  var total = 0;
-  $(".score8").each(function () {
-    total += Number($(this).val());
-  });
-  $('input[name="totalScore"]').val(total);
-}
-
-function autoCal10() {
-  var total = 0;
-  $(".score10").each(function () {
-    total += Number($(this).val());
-  });
-  $('input[name="totalScore"]').val(total);
-}
-
-function autoCal11() {
-  var total = 0;
-  $(".score11").each(function () {
-    total += Number($(this).val());
-  });
-  $('input[name="totalScore"]').val(total);
-}
-
-function autoCal12() {
-  var total = 0;
-  $(".score12").each(function () {
-    total += Number($(this).val());
-  });
-  $('input[name="totalScore"]').val(total);
-}
-
-function autoCal13() {
-  var total = 0;
-  $(".score13").each(function () {
-    total += Number($(this).val());
-  });
-  $('input[name="totalScore"]').val(total);
-}
-
-function autoCal14() {
-  var total = 0;
-  $(".score14").each(function () {
-    total += Number($(this).val());
-  });
-  $('input[name="totalScore"]').val(total);
-}
-
-//modal
-
-const modal = document.getElementById("modal");
-
-function modalOn() {
-  modal.style.display = "flex";
-}
-function isModalOn() {
-  return modal.style.display === "flex";
-}
-function modalOff() {
-  modal.style.display = "none";
-}
-const btnModal = document.getElementById("btn-modal");
-
-btnModal.addEventListener("click", (e) => {
-  modalOn();
-});
-const closeBtn = modal.querySelector(".close-area");
-
-closeBtn.addEventListener("click", (e) => {
-  modalOff();
-});
-modal.addEventListener("click", (e) => {
-  const evTarget = e.target;
-  if (evTarget.classList.contains("modal-overlay")) {
-    modalOff();
-  }
-});
-window.addEventListener("keyup", (e) => {
-  if (isModalOn() && e.key === "Escape") {
-    modalOff();
-  }
-});
