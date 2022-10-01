3687be852548c5b0593a65b45a4c32e745004532
dclxxi <dclxxi@kakao.com>
2022-10-01 18:02:59 +0900

    feat: modify incident level & place to enum string on training result check page

--- backend/src/main/java/com/port/accident/portaccident/enums/IncidentLevel.java
+++ backend/src/main/java/com/port/accident/portaccident/enums/IncidentLevel.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum IncidentLevel {
-    LEVEL_1("1"), LEVEL_2("2"), LEVEL_3("3");
+    LEVEL_1("Level1"), LEVEL_2("Level2"), LEVEL_3("Level3");
 
     private final String levelValue;
 }
