5fe111a29f9ab48efd8ad19261866f602b7c3cc5
dclxxi <dclxxi@kakao.com>
2022-10-15 18:57:56 +0900

    feat: modify html and javaScript for register emergency contact

---
+++ src/main/java/com/port/accident/portaccident/dto/training_scenario/ScenarioAccidentPortFacilityDto.java
@@ -0,0 +1,35 @@
+package com.port.accident.portaccident.dto.training_scenario;
+
+
+import com.port.accident.portaccident.enums.*;
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Data
+public class ScenarioAccidentPortFacilityDto {
+
+    private Integer id;
+    private String name;
+    private IncidentLevel incidentLevel;
+    private IncidentImpact incidentImpact;
+    private IncidentType incidentType;
+    private IncidentDetailType incidentDetailType;
+    private TrainingPlace portArea;
+    private List<PortFacility> accidentPortFacilityList = new ArrayList<>();
+
+    @QueryProjection
+    @Builder
+    public ScenarioAccidentPortFacilityDto(Integer id, String name, IncidentLevel incidentLevel, IncidentImpact impact,
+                                           IncidentType incidentType, IncidentDetailType incidentDetailType, TrainingPlace portArea) {
+        this.id = id;
+        this.name = name;
+        this.incidentLevel = incidentLevel;
+        this.incidentImpact = impact;
+        this.incidentType = incidentType;
+        this.incidentDetailType = incidentDetailType;
+        this.portArea = portArea;
+    }
+}
